799dcd2526570b9bb49d2eee3663fa3acaa5ec5a
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-03-26 00:13:24 +0100

    update

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,14 @@
+import styles from './Modal.css';
+
+
+const Modal = () => {
+    return (  
+        <div className={styles.overlay}>
+  <div className={styles.modal}>
+    <img src="" alt="" />
+  </div>
+</div>
+    );
+}
+ 
+export default Modal;
